838aeeea85fc95f57df97200ebeca0c111bbc527
hust-wuwenchao <2591099581@qq.com>
2018-06-26 22:13:25 +0800

    aaa

--- cpp-package/example/mlp_cpu.cpp
+++ cpp-package/example/mlp_cpu.cpp
@@ -53,7 +53,7 @@ int main(int argc, char** argv) {
   const int image_size = 28;
   const std::vector<int> layers{128, 64, 10};
   const int batch_size = 1;
-  const int max_epoch = 10;
+  const int max_epoch =  1;
   const float learning_rate = 0.1;
   const float weight_decay = 1e-2;
 
@@ -137,7 +137,7 @@ int main(int argc, char** argv) {
       // Update parameters
       for (size_t i = 0; i < arg_names.size(); ++i) {
         if (arg_names[i] == "X" || arg_names[i] == "label") continue;
-        
+
         opt->Update(i, exec->arg_arrays[i], exec->grad_arrays[i]);
       }
     }
